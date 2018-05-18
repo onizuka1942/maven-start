e592abaa503f6f1061dd71373c6ed38777750dfe
aziz <blade1942@hotmail.fr>
2018-05-19 01:03:27 +0200

    ajout dependency

--- src/main/java/clininc/programming/training/Application.java
+++ src/main/java/clininc/programming/training/Application.java
@@ -2,9 +2,15 @@ package clinic.programming.training;
 
 import java.util.ArrayList;
 import java.util.List;
-
+import org.apache.commons.lang3.StringUtils;
 public class Application {
     
+
+    public int countWords(String words){
+	String[] separateWords = StringUtils.split(words,' ');
+	return (separateWords ==null) ? 0 : separateWords.length;
+    }	    
+
     public void greet(){
 	List<String> greetings = new ArrayList<>();
 	greetings.add("hello");
@@ -24,5 +30,7 @@ public class Application {
     	System.out.println ("Starting Application");
 	Application app = new Application();
 	app.greet();
+	int count = app.countWords("bonjour combine de mots");
+	System.out.println("word count: "+count);
     }
 }
